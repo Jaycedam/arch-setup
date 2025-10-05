dc56d3a07e444ec7d6df2cd8a0ada510da05bd24
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2025-10-05 17:24:36 -0300

    chore: todo comments

--- main.sh
+++ main.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+# todo: get rootdir for this script to load modules
+# todo: install paru
+# todo: load module to install packages 
+# todo: uncommend color and multilib from /etc/pacman.conf
+# todo: clone dotfiles repo
+
 # change default shell to fish
 chsh -s /usr/bin/fish
 
-# install packages
-
-
-# todo: uncommend color and multilib from /etc/pacman.conf
