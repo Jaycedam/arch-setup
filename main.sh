b88519c5a477f19552f541fad32abd9a0658e4b4
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2025-10-05 18:08:12 -0300

    feat: source dotfiles script and fix paru order

--- main.sh
+++ main.sh
@@ -3,11 +3,11 @@
 ROOT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 MODULES_DIR="${ROOT_DIR}/modules"
 
-source "${MODULES_DIR}/apps.sh"
 source "${MODULES_DIR}/paru.sh"
+source "${MODULES_DIR}/apps.sh"
+source "${MODULES_DIR}/dotfiles.sh"
 
 # todo: uncommend color and multilib from /etc/pacman.conf
-# todo: clone dotfiles repo
 
 # change default shell to fish
 chsh -s /usr/bin/fish
