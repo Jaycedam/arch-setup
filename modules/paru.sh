886207875f2e65b2ad53bc044956e4369ca92daa
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2025-10-07 05:40:55 -0300

    feat: check before installing paru

--- modules/paru.sh
+++ modules/paru.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-echo "Installing paru..."
+# Check if paru is installed
+if command -v paru >/dev/null 2>&1; then
+    echo "paru is already installed, skipping installation."
+else
+    echo "Installing paru..."
+    sudo pacman -S --needed git base-devel
+    git clone https://aur.archlinux.org/paru.git $HOME/paru
+    cd $HOME/paru
+    makepkg -si
+    cd -
+    echo "Done!"
+fi
 
-sudo pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
