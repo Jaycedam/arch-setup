7f7250b2a034410688e7236c47b5028bf99c31c8
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2025-10-09 15:24:24 -0300

    feat: added more packages

--- modules/apps.sh
+++ modules/apps.sh
@@ -1,18 +1,47 @@
 #!/usr/bin/env bash
 
+# todo: separate on files per category
 PACKAGES=(
     ### cli ###
     fish
+    btop
     fzf
+    tldr
     tmux
+    ripgrep
     zoxide
     yt-dlp
+    ffmpeg
     yazi
+    bat
     fd
     kanata-bin
+    openssh
+    dmidecode
+    imagemagick
+    typst
+    pacman-contrib
+
+    # desktop
+    signal-desktop
+    proton-pass-bin
+    cryptomator-bin
+    gimp
+    localsend-bin
+    ente-desktop-bin
+    mpv
+    libreoffice-still
+    grayjay-bin
+    transmission-qt
+    ventoy-bin
+
+    # terminal emulators
+    kitty
+    ghostty
 
     ### browser ###
     brave-bin
+    zen-browser-bin
 
     ### gaming ###
     steam
@@ -32,24 +61,52 @@ PACKAGES=(
     hypridle
     hyprpolkitagent
     pavucontrol
+    rofi
 
     ### dev ###
     # tools
     git
+    bun-bin
     tree-sitter-cli
     lazygit
     nodejs
     docker
     docker-compose
+    android-tools
     github-cli
+    # ai
+    opencode-bin
+    gemini-cli-bin
     # editors
     neovim
     helix
+    zed
+    # languages
+    go
+
     # lsp
     pyright
+    vscode-langservers-extracted
     typescript-language-server
+    rust-analyzer
+    astrojs-language-server
     gopls
     bash-language-server
+    marksman
+    tailwindcss-language-server
+    yaml-language-server
+    lua-language-server
+    taplo-cli
+    # formatters
+    prettier
+    ruff
+    python-djlint
+    shfmt
+    stylua
+    python-black
+    # linters
+    eslint
+    shellcheck
 )
 
 # Update package database and upgrade system
