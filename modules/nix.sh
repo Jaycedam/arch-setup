786d9be62a4cc783d4596232a35ec2794881a6bf
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2025-12-19 21:38:48 -0300

    chore: nix pkgs list

---
+++ modules/nix.sh
@@ -0,0 +1,120 @@
+### cli ###
+impala # wifi tui selector
+fish
+btop
+fzf
+trash-cli
+tldr
+tmux
+ripgrep
+zoxide
+yt-dlp
+yazi
+bat
+fd
+kanata
+jq
+imagemagick
+typst
+lsfg-vk
+grim slurp
+fwupd
+fastfetch
+sunshine
+
+# desktop
+signal-desktop
+gimp
+mpv
+libreoffice-fresh
+transmission_4-gtk
+cryptomator
+ente-desktop-bin
+grayjay
+localsend
+xfce.thunar
+
+# terminal emulators
+kitty
+ghostty
+
+### browser ###
+brave
+
+### gaming ###
+steam
+gamemode
+gamescope
+dolphin-emu
+shadps4
+gopher64
+heroic
+protonplus
+pcsx2
+
+### fonts ###
+font-awesome
+jetbrains-mono
+noto-fonts
+noto-fonts-color-emoji
+
+# gtk themes
+graphite-gtk-theme
+
+### hyprland and utilities ###
+hyprland hyprlock hypridle hyprsunset hyprpaper hyprpolkitagent
+waybar
+dunst
+xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
+udiskie
+pavucontrol
+rofi
+nwg-look
+uwsm
+gnome-keyring
+
+### dev ###
+# tools
+git
+bun
+tree-sitter
+lazygit
+nodejs_24
+docker
+docker-compose
+android-tools
+gh
+# ai
+opencode
+gemini-cli
+# editors
+# neovim # waiting for native package manager availability
+neovim
+helix
+zed-editor
+# languages
+go
+
+# lsp
+pyright
+vscode-langservers-extracted
+typescript-language-server
+rust-analyzer
+astro-language-server
+gopls
+bash-language-server
+marksman
+tailwindcss-language-server
+yaml-language-server
+lua-language-server
+taplo
+# formatters
+prettier
+ruff
+djlint
+shfmt
+stylua
+black
+# linters
+eslint
+shellcheck
