d57b64597cd9051ed19276d1ac17146d8c0c3f9c
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2025-12-19 21:38:34 -0300

    fix: remove --now flag from systemd services to avoid errors on tty

--- modules/uwsm.sh
+++ modules/uwsm.sh
@@ -3,8 +3,8 @@
 # start services with systemd when using uwsm
 
 echo "Enabling services with systemd..."
-systemctl --user enable --now waybar.service
-systemctl --user enable --now hyprpaper.service
-systemctl --user enable --now hypridle.service
-systemctl --user enable --now hyprpolkitagent.service
-systemctl --user enable --now hyprsunset.service
+systemctl --user enable waybar.service
+systemctl --user enable hyprpaper.service
+systemctl --user enable hypridle.service
+systemctl --user enable hyprpolkitagent.service
+systemctl --user enable hyprsunset.service
